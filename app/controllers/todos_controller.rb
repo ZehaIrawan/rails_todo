68fc93350403637decb2f7a61245fa66caa01e28
Zeha <zehairawan@gmail.com>
2023-09-10 16:40:45 +0800

    use turbo remove

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :show, :toggle, :destroy]
+
   def index
     @todos = current_user.todos.all
     @todo = current_user.todos.new
@@ -9,22 +11,14 @@ class TodosController < ApplicationController
   end
 
   def new
-    # @todo = Todo.new
     @todo = current_user.todos.new
   end
 
   def create
     @todo = current_user.todos.new(todo_params)
 
-    respond_to do |format|
-      if @todo.save
-        format.turbo_stream
-        # format.html { redirect_to todo_url(@todo), notice: "todo was successfully created." }
-        # format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+    unless @todo.save
+      head :unprocessable_entity
     end
   end
 
@@ -52,12 +46,11 @@ class TodosController < ApplicationController
   end
 
   def destroy
-    @todo = current_user.todos.find(params[:id])
     @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
 
   def todo_params
